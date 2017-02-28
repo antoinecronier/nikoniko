d6f596845ea6c75f12bb13fff586d64f581713db
romain29 <manach.r@msn.com>
2017-02-28 20:05:37 +0100

    Add IDBManager

--- src/com/tactfactory/nikoniko/manager/database/manager/interfaces/INikoNikoDBManager.java
+++
@@ -1,9 +0,0 @@
-package com.tactfactory.nikoniko.manager.database.manager.interfaces;
-
-import com.tactfactory.nikoniko.manager.database.manager.interfaces.base.IDBManagerBase;
-
-public interface INikoNikoDBManager<T> extends IDBManagerBase {
-	
-	public T getById(long id);
-
-}
